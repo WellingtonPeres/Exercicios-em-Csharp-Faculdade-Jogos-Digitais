a7f2fa8181221a6f97a2b7f659fa1e4a1a7d4c23
Wellington Peres <wellingtonpv.100@gmail.com>
2022-07-26 05:53:59 -0300

    Divisão de dois inteiros

---
+++ Ex 007 - Divisão de dois inteiros/Ex 007 - Divisão de dois inteiros/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_007___Divisão_de_dois_inteiros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Desenvolva um algoritmo em C# para obter o resultado
+              da divisão de dois números inteiros quaisquer.*/
+            Console.Write("Digite um número qualquer: ");
+            int numero1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite outro número qualquer: ");
+            int numero2 = int.Parse(Console.ReadLine());
+
+            int resultado = numero1 / numero2;
+
+            Console.WriteLine($"O resultado da divisão é {resultado}");
+
+            Console.ReadLine();
+        }
+    }
+}
