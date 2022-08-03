5e55eec3de7199e1aca2e8a346c3d48370f603b6
Wellington Peres <wellingtonpv.100@gmail.com>
2022-08-03 04:38:17 -0300

    Múltiplos de 3 entre 1 e 100

---
+++ Ex 016 - Múltiplos de 3 entre 1 e 100/Ex 016 - Múltiplos de 3 entre 1 e 100/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_016___Múltiplos_de_3_entre_1_e_100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Desenvolva um programa em C# que imprima
+              todos os múltiplos de 3, entre 1 e 100.*/
+
+            for (int i = 3; i < 100; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
