a862be89acae02cc32d64cba6073d5e9a6935710
Wellington Peres <wellingtonpv.100@gmail.com>
2022-08-08 04:57:28 -0300

    Olá Mundo

---
+++ Ex 001 - Olá Mundo/Olá Mundo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olá_Mundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Desenvolva um programa em C# que imprima "Olá Mundo!"*/
+            Console.WriteLine("Olá Mundo!");
+
+            Console.ReadLine();
+        }
+    }
+}
