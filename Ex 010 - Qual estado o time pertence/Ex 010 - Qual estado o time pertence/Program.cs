e1a4317f5461b2f5299e4d8d00206a483ddb1d4c
Wellington Peres <wellingtonpv.100@gmail.com>
2022-08-08 05:00:22 -0300

    Qual estado o time pertence

---
+++ Ex 010 - Qual estado o time pertence/Ex 010 - Qual estado o time pertence/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_010___Qual_estado_o_time_pertence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Desenvolva um algoritmo em C# que informe com base no nome
+              de um time fornecido, se este trata-se de um time carioca,
+              paulista ou de outro estado.*/
+
+            string[] carioca = { "FLAMENGO", "VASCO" };
+            string[] paulista = { "SÃO PAULO", "CORINTHIAS" };
+            string[] para = { "REMO", "CASTANHAL" };
+
+            Console.Write("Digite o nome de um time Qualquer: ");
+            string nomeTime = Console.ReadLine().ToUpper().Trim();
+
+            if (carioca.Contains(nomeTime))
+            {
+                Console.WriteLine($"{nomeTime} é um time Carioca.");
+            }
+            else if (paulista.Contains(nomeTime))
+            {
+                Console.WriteLine($"{nomeTime} é um time Paulista");
+            }
+            else if (para.Contains(nomeTime))
+            {
+                Console.WriteLine($"{nomeTime} é um time do Pará");
+            }
+            else
+            {
+                Console.WriteLine($"O {nomeTime} não existe na lista de times de nenhum estado.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
