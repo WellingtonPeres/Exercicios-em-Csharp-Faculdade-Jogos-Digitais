572c5f47d5e2a5af08c0463bfe49a2829912aa0d
Wellington Peres <wellingtonpv.100@gmail.com>
2022-08-17 05:40:24 -0300

    Elementos que são o quadrado do indice

---
+++ Ex 021 - Elementos que são o quadrado do indice/Ex 021 - Elementos que são o quadrado do indice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_021___Elementos_que_são_o_quadrado_do_indice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Criar uma array de 5 números inteiros, nos quais cada elemento
+             * seja o quadrado do índice. 
+             * - Acessar o elemento 2 do array
+             * - Acessar o elemento 5 do array */
+
+            int[] arrayMumeroQuadrado = new int[5];
+            for (int i = 0; i < arrayMumeroQuadrado.Length; i++)
+            {
+                arrayMumeroQuadrado[i] = i * i;
+            }
+
+            Console.WriteLine($"O segundo elemento do array é: {arrayMumeroQuadrado[1]}");
+            Console.WriteLine($"O quinto elemento do array é: {arrayMumeroQuadrado[4]}");
+
+            Console.ReadLine();
+        }
+    }
+}
