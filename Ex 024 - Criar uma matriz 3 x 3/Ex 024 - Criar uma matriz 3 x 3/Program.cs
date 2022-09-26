499bd751396809456446801d02b4a36cdb37ded5
Wellington Peres <wellingtonpv.100@gmail.com>
2022-09-26 13:21:46 -0300

    Criar uma matriz 3 x 3

---
+++ Ex 024 - Criar uma matriz 3 x 3/Ex 024 - Criar uma matriz 3 x 3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_024___Criar_uma_matriz_3_x_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Criar uma matriz 3 x 3 pré-configurada e imprimir os valores: */
+
+            int[,] matriz = new int[3, 3];
+
+            matriz[0, 0] = 1;
+            matriz[0, 1] = 5;
+            matriz[0, 2] = 3;
+
+            matriz[1, 0] = 2;
+            matriz[1, 1] = 4;
+            matriz[1, 2] = 8;
+
+            matriz[2, 0] = 9;
+            matriz[2, 1] = 0;
+            matriz[2, 2] = 6;
+
+            for (int m = 0; m < matriz.GetLength(0); m++)
+            {
+                for (int n = 0; n < matriz.GetLength(1); n++)
+                {
+                    Console.Write(matriz[m, n] + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ex 024 - Criar uma matriz 3 x 3/Ex 024 - Criar uma matriz 3 x 3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Ex 024 - Criar uma matriz 3 x 3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ex 024 - Criar uma matriz 3 x 3")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("72b3b2f7-be57-4b7c-a19d-f1d13d7bf0e5")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
