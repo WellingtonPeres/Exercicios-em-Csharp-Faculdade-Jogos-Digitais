97f07523bd52e222b2872aaac230318aa3d8bd30
Wellington Peres <wellingtonpv.100@gmail.com>
2023-01-27 08:21:57 -0300

    Criar um método que retorna seu nome

---
+++ Ex 029 - Criar um método que retorna seu nome/Ex 029 - Criar um método que retorna seu nome/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_029___Criar_um_método_que_retorna_seu_nome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Criar um método que imprima na tela o nome de uma pessoa
+             * - Identifique a assinatura do método criado.
+             * - Identifique o retorno do método criado. */
+
+            Console.Write("Escreva um nome qualquer: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine(RetornarNomePessoa(nome));
+
+            Console.ReadLine();
+        }
+
+        public static string RetornarNomePessoa(string nome)
+        {
+            return nome;
+        }
+    }
+}
